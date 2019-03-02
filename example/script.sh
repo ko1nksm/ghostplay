acd808fa49ebd5f67dab04a5f816f0b4f27e30db
Koichi Nakashima <koichi@nksm.name>
2019-03-02 18:25:08 +0900

    Improve example

--- example/script.sh
+++ example/script.sh
@@ -1,23 +1,4 @@
 #!/bin/sh
 
-: Usage ghostplay example/script.sh
-echo This is ghostplay example
-
-# ghostplay silent
-echo silent
-# ghostplay end
-
-# ghostplay batch
-cat<<DATA
-multi line test 1
-multi line test 2
-multi line test 3
-DATA
-# ghostplay flush
-echo flush at before this line
-# ghostplay end
-
-
-# ghostplay fake
-echo fake
-# ghostplay end
+echo This is your script.
+echo ghostplay types your script and execute.
