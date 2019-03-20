c49a7be5bb033d02b687cc95d5a7f03eb8e8395c
Koichi Nakashima <koichi@nksm.name>
2019-03-21 01:15:36 +0900

    Use Data helper for test

--- spec/ghostplay_spec.sh
+++ spec/ghostplay_spec.sh
@@ -69,40 +69,39 @@ Describe 'ghostplay'
   End
 
   Describe 'ghostplay_input()'
-    Before n=0
-    Example 'types 5 characters when given the string ABCDE'
-      ghostplay_type() { echo "$1"; n=$((n+1)); }
-      When call ghostplay_input ABCDE
+    Example 'types characters'
+      ghostplay_type() { echo "$1"; }
+      When call ghostplay_input "ABCDE"
       The line 1 of output should equal 'A'
       The line 2 of output should equal 'B'
       The line 3 of output should equal 'C'
       The line 4 of output should equal 'D'
       The line 5 of output should equal 'E'
-      The variable n should equal 5
+      The lines of output should equal 5
     End
   End
 
   Describe 'ghostplay_type()'
     Example 'outputs string'
-      When call ghostplay_type abcde
+      When call ghostplay_type 'abcde'
       The output should equal 'abcde'
     End
 
     Example 'calls ghostplay_before_type_hook before type'
-      ghostplay_before_type_hook() { shellspec_puts before_type; }
-      When call ghostplay_type ' abcde'
-      The output should equal 'before_type abcde'
+      ghostplay_before_type_hook() { shellspec_puts "prepend-"; }
+      When call ghostplay_type 'abcde'
+      The output should equal 'prepend-abcde'
     End
 
     Example 'calls ghostplay_after_type_hook after type'
-      ghostplay_after_type_hook() { shellspec_puts after_type; }
-      When call ghostplay_type 'abcde '
-      The output should equal 'abcde after_type'
+      ghostplay_after_type_hook() { shellspec_puts "-append"; }
+      When call ghostplay_type 'abcde'
+      The output should equal 'abcde-append'
     End
   End
 
   Describe 'ghostplay_command_and_exec()'
-    Example 'output command and exec'
+    Example 'outputs command and exec'
       When call ghostplay_command_and_exec "echo 123"
       The first line of output should equal 'echo 123'
       The second line of output should equal '123'
@@ -111,7 +110,7 @@ Describe 'ghostplay'
   End
 
   Describe 'ghostplay_command_and_fake()'
-    Example 'output command and not exec'
+    Example 'outputs command and not exec'
       When call ghostplay_command_and_fake "echo 123"
       The first line of output should equal 'echo 123'
       The second line of output should equal '$ '
@@ -119,32 +118,26 @@ Describe 'ghostplay'
   End
 
   Describe 'ghostplay_run()'
-    run_script() { script | ghostplay_run; }
-    Example 'execute by line'
-      script() {
-        printf '%s\n' \
-          'echo 1' \
-          'echo 2' \
-          ''
-      }
-
-      When call run_script
+    Example 'executes by each line'
+      Data
+        #|echo 1
+        #|echo 2
+      End
+      When call ghostplay_run
       The line 1 of output should equal '$ echo 1'
       The line 2 of output should equal '1'
       The line 3 of output should equal '$ echo 2'
       The line 4 of output should equal '2'
     End
 
-    Example 'execute with batch'
-      script() {
-        printf '%s\n' \
-          '#ghostplay batch' \
-          'echo 1' \
-          'echo 2' \
-          '#ghostplay end' \
-          ''
-      }
-      When call run_script
+    Example 'executes with batch'
+      Data
+        #|#ghostplay batch
+        #|echo 1
+        #|echo 2
+        #|#ghostplay end
+      End
+      When call ghostplay_run
       The line 1 of output should equal '$ echo 1'
       The line 2 of output should equal 'echo 2'
       The line 3 of output should equal '1'
@@ -152,17 +145,15 @@ Describe 'ghostplay'
       The line 5 of output should equal '$ '
     End
 
-    Example 'execute with batch and flush'
-      script() {
-        printf '%s\n' \
-          '#ghostplay batch' \
-          'echo 1' \
-          '#ghostplay flush' \
-          'echo 2' \
-          '#ghostplay end' \
-          ''
-      }
-      When call run_script
+    Example 'executes with batch and flush'
+      Data
+        #|#ghostplay batch
+        #|echo 1
+        #|#ghostplay flush
+        #|echo 2
+        #|#ghostplay end
+      End
+      When call ghostplay_run
       The line 1 of output should equal '$ echo 1'
       The line 2 of output should equal '1'
       The line 3 of output should equal '$ echo 2'
@@ -170,44 +161,38 @@ Describe 'ghostplay'
       The line 5 of output should equal '$ '
     End
 
-    Example 'execute with silent'
-      script() {
-        printf '%s\n' \
-          '#ghostplay silent' \
-          'echo 1' \
-          'echo 2' \
-          '#ghostplay end' \
-          ''
-      }
-      When call run_script
+    Example 'executes with silent'
+      Data
+        #|#ghostplay silent
+        #|echo 1
+        #|echo 2
+        #|#ghostplay end
+      End
+      When call ghostplay_run
       The line 1 of output should equal '1'
       The line 2 of output should equal '2'
       The line 3 of output should be blank
     End
 
-    Example 'execute with fake'
-      script() {
-        printf '%s\n' \
-          '#ghostplay fake' \
-          'echo 1' \
-          'echo 2' \
-          '#ghostplay end' \
-          ''
-      }
-      When call run_script
+    Example 'executes with fake'
+      Data
+        #|#ghostplay fake
+        #|echo 1
+        #|echo 2
+        #|#ghostplay end
+      End
+      When call ghostplay_run
       The line 1 of output should equal '$ echo 1'
       The line 2 of output should equal 'echo 2'
       The line 3 of output should equal '$ '
     End
 
-    Example 'execute with sleep'
+    Example 'executes with sleep'
       sleep() { echo "$@"; }
-      script() {
-        printf '%s\n' \
-          '#ghostplay sleep 5' \
-          ''
-      }
-      When call run_script
+      Data
+        #|#ghostplay sleep 5
+      End
+      When call ghostplay_run
       The output should equal 5
     End
   End
